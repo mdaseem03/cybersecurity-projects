a37ede3774d03fcb95419bde1062353e594146d1
mdaseem03 <98540960+mdaseem03@users.noreply.github.com>
2023-03-22 22:29:08 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -1,11 +1,15 @@
 #! /usr/bin/python3
 #python3 jarvis.py
 import time
-import whois
+import whois  
 import sys
 import requests
 from concurrent.futures import ThreadPoolExecutor
 from pwn import *
+from termcolor import *
+import nmap
+import socket
+import hashlib
 
 
 def intro():
@@ -35,13 +39,14 @@ def display_message():
     print("[2] Directory Enumeration")
     print("[3] Subdomain Enumeration")
     print("[4] Port Scanning")
-    print("[5] Full Reconnaisance and Scanning")
     print("\n")
     print("#"*60)
     print(" \t\t\tEXPLOITATION ")
     print("#"*60)
-    print("[6] SHA256 Cracker")
+    print("[5] SHA256 Cracker")
     print("\n")
+    print("\n")
+    print("[0] Exit")
     
 def whois_lookup():
     print("\033[1;37m") #bold white    
@@ -53,6 +58,7 @@ def whois_lookup():
     domain = input(">> Enter Domain Name: ")
     print("\n")
     print("-"*60)
+    print(domain)
     
     w = whois.whois(domain)
     print("Domain Name: {}".format(w.domain_name))
@@ -76,17 +82,26 @@ def direnum():
     print("#"*60)
     print(" \t\t\tDIRECTORY ENUMERATION ")
     print("#"*60)
+    print("\u001b[0m")
 
-    url = input(">> Enter URL: ")
-    dir_list = input(">> Enter Wordlist: ").split('\n')
+    url = input(">> Enter URL: ").strip()
+    dir_list = input(">> Enter Wordlist: ").strip()
     concurrent = 50
 
     def check_directory(directory):
         directory = directory.strip()  # remove any whitespace
-        full_url = url + directory
+        full_url = url + "/" + directory
         response = requests.get(full_url)
-        if response.status_code != 404:
-            print("{} : Status Code [{}]".format(full_url,response.status_code))
+        status_code = response.status_code
+        if status_code != 404:
+            if 300 <= status_code < 400:
+                print(colored("{} : Status Code [{}]".format(full_url,status_code), 'orange'))
+            elif status_code == 200:
+                print(colored("{} : Status Code [{}]".format(full_url,status_code), 'green'))
+            elif 400 <= status_code < 500 and status_code != 404:
+                print(colored("{} : Status Code [{}]".format(full_url,status_code), 'yellow'))
+            else:
+                print(colored("{} : Status Code [{}]".format(full_url,status_code), 'red'))
 
     with log.progress("Bruteforcing directories at {} \n".format(url)) as p:
         with ThreadPoolExecutor(max_workers=concurrent) as executor:
@@ -95,9 +110,117 @@ def direnum():
                 for future in futures:
                     future.result()
             except KeyboardInterrupt:
+                sys.exit(1)
+        p.success("Directories Enumeration Completed")
+
+
+def subdomain_enum():
+    print("\033[1;37m") #bold white    
+    print("#"*60)
+    print(" \t\t\tSUBDOMAIN ENUMERATION ")
+    print("#"*60)
+    print("\u001b[0m")
+
+    domain = input(">> Enter Domain: ").strip()
+    wordlist = input(">> Enter Wordlist: ").strip()
+    print('\n')
+    
+    sub = open(wordlist,'r')
+    
+    with log.progress("Enumerating subdomains for {} \n".format(domain)) as p:
+        def enum(subdomain):
+            global count
+            count = 0 #used to print final success or failure message
+            subdomain = subdomain.strip() # remove whitespace and newline characters
+            url = "{}.{}".format(subdomain,domain)
+
+            try:
+                ip = socket.gethostbyname(url)
+            except Exception:
+                pass
+            else:
+                print("[*] {} [{}]".format(url,ip))
+                count += 1
+
+        with ThreadPoolExecutor() as executor:
+            futures = [executor.submit(enum, subdomain) for subdomain in sub]
+            try:
+                for future in futures:
+                    future.result()
+            except KeyboardInterrupt:
                 p.status("Received KeyboardInterrupt. Stopping threads...")
                 executor.shutdown(wait=False)
-        p.success("Directories Enumeration Completed")
+            if count!=0:
+                p.success("Subdomains found: ")
+            else:
+                p.failure("Finished !!")
+
+def scan_ports(ip, ports):
+    nm.scan(ip, ports)
+    for hosts in nm.all_hosts():
+        for proto in nm[hosts].all_protocols():
+            lport = nm[hosts][proto].keys()
+            #lport.sorted()
+            print("PORT\t\tSERVICE\t\tSTATE")
+            print("----\t\t-------\t\t-----")
+            for port in lport:
+                try:
+                    service = socket.getservbyport(port)
+                except Exception:
+                    service="unknown"
+                print("{}/{}\t\t{}\t\t{}".format(port,proto,service,nm[hosts][proto][port]['state']))
+
+def portscan():
+    global nm
+    nm = nmap.PortScanner()
+    ip = input(">> Enter [IP Address|Domain|URL]: ")
+    ports = input(">> Enter Ports Range: ")
+
+    try:
+        url = ip
+        domain = whois.whois(url).domain_name
+        ip = socket.gethostbyname(domain)
+    except TypeError:
+        ip = ip
+        
+    print("-"*20)
+    print("IP Address: ",ip)
+    print("Port Range: ",ports)
+    print("-"*20)
+
+    # Split the port range into chunks for threading
+    port_chunks = ports.split(",")
+    threads = []
+
+    for chunk in port_chunks:
+        start_port, end_port = chunk.split("-")
+        thread = threading.Thread(target=scan_ports, args=(ip, "{}-{}".format(start_port, end_port)))
+        thread.start()
+        threads.append(thread)
+
+    # Wait for all threads to finish
+    for thread in threads:
+        thread.join()
+
+def shacrack():
+    wanted_hash = input(">> Enter Hash Value: ").strip()#sha256sum is stored
+    password_file = input(">> Enter Wordlist: ").strip() #reads password file path
+    attempts = 0 #it will calc n.o. of attempts
+
+    with log.progress("Attempting to crack: {}!\n".format(wanted_hash)) as p: #for displaying progress of execution
+        with open(password_file, "r", encoding='utf-8') as password_list: #open password file(rockyou.txt) with enc=utf-8. Because for hashing, string should be encoded
+            for password in password_list: #iterates each line in password file
+                password = password.strip("\n").encode('utf-8') #password is encoded to utf-8
+                password_obj = hashlib.sha256(password) #password is converted to sha256
+                password_hash = password_obj.hexdigest()
+                p.status("[{}] {} == {}".format(attempts, password.decode('utf-8'), password_hash)) #display status of execution in each iteration
+                if password_hash == wanted_hash:
+                    p.success("Password Hash found after {} attempts! \033[1m\033[92m{}\033[0m hashes to {}!".format(attempts, password.decode('utf-8'), password_hash))
+                    exit()
+                attempts +=1
+            p.failure("Password Hash not found !")
+
+
 
 
 
@@ -119,6 +242,14 @@ if __name__ == "__main__":
         whois_lookup()
     elif ch == 2:
         direnum()
-
+    elif ch == 3:
+        subdomain_enum()
+    elif ch == 4:
+        portscan()
+    elif ch == 5:
+        shacrack()
+    elif ch == 0:
+        print("See You Again Chief !! ...")
+        sys.exit(1)
 
 

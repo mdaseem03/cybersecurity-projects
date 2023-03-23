a1e344e384980222a8b2bf265482514db34fa8ce
mdaseem03 <98540960+mdaseem03@users.noreply.github.com>
2023-03-23 12:13:47 +0530

    Update and rename req.txt to installl.sh

--- installl.sh
+++ installl.sh
@@ -1,5 +1,6 @@
-apt-get update
-apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
+#! /bin/sh
+sudo apt-get update
+sudo apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade pwntools
 python3 -m unittest discover test

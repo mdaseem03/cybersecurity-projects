699477cb94955c16399915b97d2b09f596eb1240
mdaseem03 <98540960+mdaseem03@users.noreply.github.com>
2023-03-23 11:47:45 +0530

    Update hiddir.py

--- hiddir.py
+++ hiddir.py
@@ -5,16 +5,19 @@ from pwn import *
 from termcolor import *
 
 
-def direnum():
-    url = input(">> Enter URL: ").strip()
-    dir_list = input(">> Enter Wordlist: ").strip()
-    concurrent = 50
+if len(sys.argv) != 5:
+    print('Usage: python3 hiddir.py -u <url> -d <dir wordlist>')
+    sys.exit(1)
 
-    def check_directory(directory):
-        directory = directory.strip()  # remove any whitespace
-        full_url = url + "/" + directory
-        response = requests.get(full_url)
-        status_code = response.status_code
+url = sys.argv[2]
+dir_list = sys.argv[4]
+concurrent = 50
+
+def check_directory(directory):
+    directory = directory.strip()  # remove any whitespace
+    full_url = url + "/" + directory
+    response = requests.get(full_url)
+    status_code = response.status_code
         if status_code != 404:
             if 300 <= status_code < 400:
                 print(colored("{} : Status Code [{}]".format(full_url,status_code), 'orange'))
@@ -25,15 +28,14 @@ def direnum():
             else:
                 print(colored("{} : Status Code [{}]".format(full_url,status_code), 'red'))
 
-    with log.progress("Bruteforcing directories at {} \n".format(url)) as p:
-        with ThreadPoolExecutor(max_workers=concurrent) as executor:
-            futures = [executor.submit(check_directory, directory) for directory in open(dir_list)]
-            try:
-                for future in futures:
-                    future.result()
-            except KeyboardInterrupt:
-                sys.exit(1)
-        p.success("Directories Enumeration Completed")
 
-direnum()
-
+with log.progress("Bruteforcing directories at {} \n".format(url)) as p:
+    with ThreadPoolExecutor(max_workers=concurrent) as executor:
+        futures = [executor.submit(check_directory, directory) for directory in open(dir_list)]
+        try:
+            for future in futures:
+                future.result()
+        except KeyboardInterrupt:
+            p.status("Received KeyboardInterrupt. Stopping threads...")
+            executor.shutdown(wait=False)
+    p.success("Directories Enumeration Completed")
